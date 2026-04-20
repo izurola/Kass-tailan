85c666aa80a726defec7b4bf41bc8951e0b7c421
Biligt.G <62086754+izurola@users.noreply.github.com>
2026-04-20 03:28:14 +0000

    initialp

---
+++ tailan.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+df = pd.read_excel("sales.xlsx")
+
+total_patties = 0
+total_buns = 0
+total_cheese = 0
+
+for _, row in df.iterrows():
+    item = row['Item Name'].lower()
+        qty = row['Quantity']
+
+            if "burger" in item:
+                    total_buns += qty
+
+                            if "chicken" not in item:
+                                        if "(xl)" in item:
+                                                        total_patties += 2 * qty
+                                                                    elif "(l)" in item:
+                                                                                    total_patties += 1 * qty
+
+                                                                                        if "cheddar" in item or "chedar" in item:
+                                                                                                total_cheese += qty
+
+                                                                                                print("Patties:", total_patties)
+                                                                                                print("Buns:", total_buns)
+                                                                                                print("Cheese slices:", total_cheese)
